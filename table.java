5ee44edbef11fc98d8ec231359d7bb1f7cb7503d
Prajwal <prajwalganar59@gmail.com>
2025-01-10 09:35:24 +0530

    Tables code added

---
+++ table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class table {
+    public static void main(String[] args) {
+        
+  
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int i=1;
+    do{
+        System.out.println(num+" * "+i+" = "+num*i);
+        i++;
+    }while(i<=10);
+}
+}
